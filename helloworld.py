58f1225e7a08fca64081753318eb584760c41366
shashank reddy sunkara <inusasunny@gmail.com>
2022-11-14 21:37:10 +0100

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -3,3 +3,5 @@ import sys
 print ('Hello','World', sep=', ', end='!\n', file=sys.stdout, flush=True)
 print (7+7,7+7,sep='+',end='\n',file=sys.stdout,flush=True)
 print ('Hello, World!')
+name = input ("Enter name")
+print("Oh yeahhh" + ' ' + name)
